613314f2a26ae20e262fb8c6ca0b6d12871dae00
khadafi_rohman <dafi.pri19@Agmail.com>
2019-06-20 11:32:29 +0700

    develop destiniasi

---
+++ app/src/main/java/id/hike/apps/android_mpos_mumu/NextDestinasiSekaliJalan.java
@@ -0,0 +1,14 @@
+package id.hike.apps.android_mpos_mumu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NextDestinasiSekaliJalan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_destinasi_sekali_jalan);
+    }
+}
